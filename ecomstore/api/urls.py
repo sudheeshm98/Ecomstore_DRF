7520c4db15a906b39cfc7d7f9362170e8507c4ce
Sudheesh M <vpzsudheesh@gmail.com>
2024-02-06 14:20:10 +0530

    commit

--- ecomstore/api/urls.py
+++ ecomstore/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ListSmartphones
+from .views import ListSmartphones, DetailView
 from . import views
 
 urlpatterns = [
-    path('smartphones/', ListSmartphones.as_view(), name='smartphones')
+    path('smartphones/', ListSmartphones.as_view(), name='smartphones'),
+    path('details/<int:pk>/', DetailView.as_view(), name='details')
+
 
 ]
--- ecomstore/api/views.py
+++ ecomstore/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 
 from .models import Smartphones
 from .serializers import SmartphoneSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 
 # Create your views here.
@@ -17,3 +18,15 @@ class ListSmartphones(ListCreateAPIView):
         queryset = self.get_queryset()
         serializer = SmartphoneSerializer(queryset, many=True)
         return Response(serializer.data)
+
+class DetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = SmartphoneSerializer
+    queryset = Smartphones.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_object()
+        if queryset is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SmartphoneSerializer(queryset,many=False)
+        return Response(serializer.data)
+
